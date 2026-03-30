0d4e473521d8ea98fd859185c86d427e39c9c3b4
barik-bijayalaxmi <barikbijayalaxmi066@gmail.com>
2026-03-30 16:02:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
+
 const API_BASE_URL = "https://api.theawsn.shop";
 
-//const API_BASE_URL = "https://localhost:4000"; 
-
-
-// ================= REGISTER =================
 async function registerUser() {
   const name = document.getElementById("regName").value;
   const email = document.getElementById("regEmail").value;
@@ -22,64 +19,7 @@ async function registerUser() {
     document.getElementById("result").innerText =
       JSON.stringify(data, null, 2);
 
-  } catch (err) {
-    console.error("Register error:", err);
+  } catch (error) {
+    console.error("Full error:", error);
   }
-}
-
-
-// ================= LOGIN =================
-async function loginUser() {
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
-
-  try {
-    const res = await fetch(
-      "https://microservices-alb-1601462659.us-east-1.elb.amazonaws.com/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      }
-    );
-
-    const data = await res.json();
-    document.getElementById("result").innerText =
-      JSON.stringify(data, null, 2);
-
-  } catch (err) {
-    console.error("Login error:", err);
-  }
-}
-
-
-// ================= LOAD USERS =================
-async function loadUsers() {
-  try {
-    const res = await fetch(`${API_BASE_URL}/users`);
-    const data = await res.json();
-
-    document.getElementById("result").innerText =
-      JSON.stringify(data, null, 2);
-
-  } catch (err) {
-    console.error("Users error:", err);
-  }
-}
-
-
-// ================= LOAD ORDERS =================
-async function loadOrders() {
-  try {
-    const res = await fetch(`${API_BASE_URL}/orders`);
-    const data = await res.json();
-
-    document.getElementById("result").innerText =
-      JSON.stringify(data, null, 2);
-
-  } catch (err) {
-    console.error("Orders error:", err);
-  }
-}
+} 
